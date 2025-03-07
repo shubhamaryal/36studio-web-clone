823c48ba7e31f836daaeafe7b19cbaaf992a6b82
shubhamaryal <aryalshubham62@gmail.com>
2025-03-07 12:41:30 +0545

    add all canvas of all page with proper size, and position

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import Canvas from './Canvas'
+import React, { useEffect } from "react";
+import Canvas from "./Canvas";
+import data from "./data";
+import LocomotiveScroll from "locomotive-scroll"; // for smooth scrolling
 
 const App = () => {
+  useEffect(() => {
+    const locomotiveScroll = new LocomotiveScroll();
+  }, []);
+
   return (
     <>
-    <div className='w-full min-h-screen bg-black'>
-      <Canvas></Canvas>
-    </div>
+      {/* if we map the data then all the arrays will be mapped, 
+        so we need to map the data's index again to get the info of the canvas i.e. to maps the objects
+        but it will again map all the objects of all arrays so to load the cavas of only first page we mapped data[0] */}
+      <div className="w-full relative min-h-screen">
+        {data[0].map((canvasdets, index) => (
+          <Canvas details={canvasdets} />
+        ))}
+      </div>
+      <div className="w-full relative min-h-screen">
+        {data[1].map((canvasdets, index) => (
+          <Canvas details={canvasdets} />
+        ))}
+      </div>
+      <div className="w-full relative min-h-screen">
+        {data[2].map((canvasdets, index) => (
+          <Canvas details={canvasdets} />
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -3,14 +3,16 @@ import canvasImages from "./canvasimages"; // importing 149 images
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
-function Canvas() {
-  const [index, setIndex] = useState({ value: 0 }); // the useState hook indicates which image to be shown
+function Canvas({ details }) {
+  const { startIndex, numImages, duration, size, top, left, zIndex } = details; // destructured, we can use details.startIndex too in the below codes
+
+  const [index, setIndex] = useState({ value: startIndex }); // the useState hook indicates which image to be shown
   const canvasRef = useRef(null);
 
   useGSAP(() => {
     gsap.to(index, {
-      value: 149,
-      duration: 3,
+      value: startIndex + numImages - 1,
+      duration: duration,
       repeat: -1, // unlimited animation
       ease: "linear",
       onUpdate: () => {
@@ -24,23 +26,33 @@ function Canvas() {
 
   useEffect(() => {
     // console.log(canvasimages); // check if the image is loading or not
+    const scale = window.devicePixelRatio;
     const canvas = canvasRef.current; // fetches the canvas element using useRef hook so that we can work on it
-    const ctx = canvas.getContext("2d"); // ctx is a drawing tool
+    const ctx = canvas.getContext("2d"); // getContext let us perform drawing operation on the canvas in 2D environment, it we does use this, canvas would just be empty space without any drawing
     const img = new Image(); // creates new instance of Image object
     img.src = canvasImages[index.value]; // changes the image according to the index and the index is changed by gsap
     img.onload = () => {
-      // the height and width of canvas is equal to the height and width of the image
-      canvas.width = img.width;
-      canvas.height = img.height;
-      ctx.drawImage(img, 0, 0); // draw the image and 0,0 indicates the position of it
+      canvas.width = canvas.offsetWidth * scale;
+      canvas.height = canvas.offsetHeight * scale;
+      canvas.style.width = canvas.offsetWidth + "px";
+      canvas.style.height = canvas.offsetHeight + "px";
+      ctx.scale(scale, scale);
+      ctx.drawImage(img, 0, 0, canvas.offsetWidth, canvas.offsetHeight); // draw the image and 0,0 indicates the position of it
     };
   }, [index]); // when the gsap changes the value of index, the useEffect hook runs and it is because useEffect has index as dependencies
 
   return (
     <canvas
       ref={canvasRef}
-      className="w-[18rem] h-[18rem]"
-      id="canvas"
+      className="absolute"
+      style={{
+        width: `${size * 1.4}px`,
+        height: `${size * 1.4}px`,
+        top: `${top}%`,
+        left: `${left}%`,
+        zIndex: `${zIndex}`,
+      }}
+      id="canvas" // the id doesn't have any use, the canvas of useEffect and this is different
     ></canvas>
   );
 }
